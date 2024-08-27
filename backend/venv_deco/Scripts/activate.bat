7059e2f81eb87754e417d1be440fd5918fe29ef2
Jacob Habchi <Jacobhabchi11@hotmail.com>
2024-08-27 16:00:42 +1000

    Setup of frontend

--- backend/venv_deco/Scripts/activate.bat
+++ backend/venv_deco/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jacob\onedrive\artcompanionai\backend\venv_deco
+set VIRTUAL_ENV=C:\art-companion\backend\venv_deco
 
 if not defined PROMPT set PROMPT=$P$G
 
